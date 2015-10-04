be880155dfe1620b7f1c00dc87ba7715db2635c0
Tai J. Enrico <taiphoon007@gmail.com>
2015-10-04 13:40:32 -0400

    Networked Cameras, Basic Tile Collisions

---
+++ ProjectFriendlyFire/Assets/Scripts/Level.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Level : MonoBehaviour {
+
+    public Transform wallPrefab;
+
+    public enum Tile : byte
+    {
+        Empty = 0,
+        Wall,
+        SlantUpRight,
+        SlantUpLeft,
+        SlantDownLeft,
+        SlantDownRight
+    };
+
+    private List<List<Tile>> map;
+    private int mapSize = 16;
+    private const int CELL_SIZE = 48;
+
+	// Use this for initialization
+	void Start () {
+        generateMap();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void generateMap()
+    {
+        map = new List<List<Tile>>();
+        for (int i = 0; i < mapSize; i++) // row (y)
+        {
+            List<Tile> row = new List<Tile>();
+            for (int j = 0; j < mapSize; j++) // column (x)
+            {
+                Tile t = Tile.Empty;
+                if (j == 0 || i == 0 || j == mapSize - 1 || i == mapSize - 1)
+                {
+                    t = Tile.Wall;
+                    Instantiate(wallPrefab, new Vector3(j, i, 10), Quaternion.Euler(new Vector3()));
+                }
+                row.Add(t);
+            }
+            map.Add(row);
+        }
+    }
+
+    public Tile getTile(float x, float y)
+    {
+        int row = (int)Mathf.Floor(y);
+        int col = (int)Mathf.Floor(x);
+        Tile t = map[row][col];
+        float xx = x - col;
+        float yy = y - row;
+        switch (t)
+        {
+            case Tile.SlantUpRight:
+                if (yy <= (1 - xx))
+                {
+                    t = Tile.SlantUpRight;
+                }
+                break;
+            case Tile.SlantUpLeft:
+                if (yy <= xx) 
+                {
+                    t = Tile.SlantUpLeft;
+                }
+                break;
+            case Tile.SlantDownLeft:
+                if (yy >= (1 - xx))
+                {
+                    t = Tile.SlantDownLeft;
+                }
+                break;
+            case Tile.SlantDownRight:
+                if (yy >= xx)
+                {
+                    t = Tile.SlantDownRight;
+                }
+                break;
+            default:
+                break;
+        }
+        return t;
+    }
+}
--- ProjectFriendlyFire/Assets/Scripts/Player.cs
+++ ProjectFriendlyFire/Assets/Scripts/Player.cs
@@ -5,19 +5,40 @@ using System.Collections;
 
 public class Player : NetworkBehaviour {
 
-    [SyncVar] public int dx;
-    [SyncVar] public int dy;
-    public float moveSpeed = 0.04f;
+    [SyncVar] public float dx;
+    [SyncVar] public float dy;
+    public float moveSpeed = 0.02f;
+
+    private const float diag = 0.70710678118f; // sqrt(2)/2
+    public GameObject cam;
+
+    private Vector3 cameraOffset = new Vector3(0, 0, -10);
 
     #region Initialization
 
     void Start()
     {
+        this.transform.Translate(new Vector3(8, 8, 0));
+        if (isLocalPlayer) {
+            GameObject.Find("MainCamera").SetActive(false);
+            cam = new GameObject("PlayerCamera" + GameObject.FindGameObjectsWithTag("Player").Length);
+            cam.transform.Translate(0, 0, -10);
+            cam.AddComponent<Camera>();
+            cam.AddComponent<GUILayer>();
+            cam.AddComponent<FlareLayer>();
+            //o.AddComponent<AudioListener>();
+            cam.GetComponent<Camera>().clearFlags = CameraClearFlags.SolidColor;
+            cam.GetComponent<Camera>().backgroundColor = Color.black;
+            cam.GetComponent<Camera>().orthographic = true;
+            cam.GetComponent<Camera>().depth = -1;
+            cam.GetComponent<Camera>().orthographicSize = 8;
+            cam.GetComponent<Camera>().enabled = false;
+        }
     }
 
     public override float GetNetworkSendInterval()
     {
-        return 0.04f;
+        return 0.03f;
     }
 
     public override void OnStartClient()
@@ -34,11 +55,10 @@ public class Player : NetworkBehaviour {
             return;
         }
 
-        int oldDx = dx;
-        int oldDy = dy;
+        float oldDx = dx;
+        float oldDy = dy;
         dx = 0;
         dy = 0;
-
         
         if (Input.GetKey(KeyCode.W))
         {
@@ -57,6 +77,50 @@ public class Player : NetworkBehaviour {
             dx += 1;
         }
 
+        if (dx != 0 && dy != 0)
+        {
+            dx *= diag;
+            dy *= diag;
+        }
+
+        Vector3 dirA = (new Vector3(dx, dy, 0).normalized)*0.52f;
+        Vector3 dirB = Quaternion.AngleAxis(-45, new Vector3(0, 0, 1)) * dirA;
+        Vector3 dirC = Quaternion.AngleAxis(45, new Vector3(0, 0, 1)) * dirA;
+        Vector3 collisionA = transform.position + dirA;
+        Vector3 collisionB = transform.position + dirB;
+        Vector3 collisionC = transform.position + dirC;
+
+        Level lvl = GameObject.Find("LevelManager").GetComponent<Level>();
+        Level.Tile ta = lvl.getTile(collisionA.x, collisionA.y);
+        Level.Tile tb = lvl.getTile(collisionB.x, collisionB.y);
+        Level.Tile tc = lvl.getTile(collisionC.x, collisionC.y);
+
+        bool a = ta != Level.Tile.Empty;
+        bool b = tb != Level.Tile.Empty;
+        bool c = tc != Level.Tile.Empty;
+        if(a && b && c)
+        {
+            dx = 0;
+            dy = 0;
+        }
+        else if (a && b)
+        {
+            
+        }
+        else if (a && c)
+        {
+
+        }
+        else if (b)
+        {
+
+        }
+        else if (c)
+        {
+
+        }
+
+
         if (dx != oldDx || dy != oldDy)
         {
             CmdMove(dx, dy);
@@ -64,7 +128,7 @@ public class Player : NetworkBehaviour {
 	}
 
     [Command]
-    public void CmdMove(int dx, int dy)
+    public void CmdMove(float dx, float dy)
     {
         this.dx = dx;
         this.dy = dy;
@@ -75,7 +139,22 @@ public class Player : NetworkBehaviour {
     [ServerCallback]
     public void FixedUpdate()
     {
-        transform.Translate(dx * moveSpeed, dy * moveSpeed, 0);
+        float tx = dx * moveSpeed;
+        float ty = dy * moveSpeed;
+        transform.Translate(tx, ty, 0);
+    }
+
+    public void OnGUI()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        if (Event.current.type == EventType.repaint)
+        {
+            cam.transform.position = transform.position + cameraOffset;
+            cam.GetComponent<Camera>().Render();
+        }
     }
 
     #region Utility/Debug
